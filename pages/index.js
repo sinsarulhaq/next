bf846a5f16d4d2e4b52206e3c912de82219ee784
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2022-07-03 14:13:05 +0530

    Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Link from 'next/link'
+
 function Home(){
-    return <h1>home</h1>
+    return(
+        <>
+        <h1>home</h1>
+        <Link href={'/blog'}>
+        <a>blog</a>
+        </Link>
+        <Link href={'/products'}>
+            <a>products</a>
+        </Link>
+        </>
+    ) 
 }
 
 export default Home;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,32 @@
-function ProductList(){
+import Link from 'next/link'
+
+
+function ProductList({ productId = 100}){
     return(
         <>
-        <h1>Product 1</h1>
-        <h1>Product 2</h1>
-        <h1>Product 3</h1>
+        <Link href="/">
+        <a>back to home</a>
+        </Link>
+        <h2>
+        <Link href={'/products/1'}>
+            <a>Product 1</a>
+        </Link>
+        </h2>
+        <h2>
+        <Link href={'/products/2'}>
+            <a>Product 2</a>
+        </Link>
+        </h2>
+        <h2>
+        <Link href={'/products/3'} replace>
+            <a>Product 3</a>
+        </Link>
+        </h2>
+        <h2>
+        <Link href={`/products/${productId}`}>
+            <a>Product {productId}</a>
+        </Link>
+        </h2>
         </>
     )
 }
